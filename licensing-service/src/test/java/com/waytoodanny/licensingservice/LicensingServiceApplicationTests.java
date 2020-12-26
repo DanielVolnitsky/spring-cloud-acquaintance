ebfee7ee565a0fab294a4f5a6b5be2dfc723dfe5
Daniel_Volnitsky <waytodanny@gmail.com>
2020-12-26 13:42:22 +0200

    add licensing-service backbone

---
+++ licensing-service/src/test/java/com/waytoodanny/licensingservice/LicensingServiceApplicationTests.java
@@ -0,0 +1,12 @@
+package com.waytoodanny.licensingservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LicensingServiceApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+}
