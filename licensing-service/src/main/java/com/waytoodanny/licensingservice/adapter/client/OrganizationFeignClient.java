e7ed9bcf114ebd071b7334ab5cefdec345cca67c
Daniel <waytodanny@gmail.com>
2021-01-02 19:47:29 +0200

    add feign based organization service client to the licensing service

---
+++ licensing-service/src/main/java/com/waytoodanny/licensingservice/adapter/client/OrganizationFeignClient.java
@@ -0,0 +1,22 @@
+package com.waytoodanny.licensingservice.adapter.client;
+
+import com.waytoodanny.licensingservice.domain.Organization;
+import com.waytoodanny.licensingservice.service.client.OrganizationServiceClient;
+import org.aspectj.weaver.ast.Or;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import java.util.Optional;
+
+@FeignClient("organizationservice")
+public interface OrganizationFeignClient extends OrganizationServiceClient {
+
+  @RequestMapping(
+      method = RequestMethod.GET,
+      value = "/v1/organizations/{organizationId}",
+      consumes = "application/json"
+  )
+  Optional<Organization> organization(@PathVariable("organizationId") String organizationId);
+}
